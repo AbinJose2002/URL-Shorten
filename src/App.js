f363165309dfc0af2b671db78189987e893c1626
abinjos307@gmail.com <abinjos307@gmail.com>
2024-05-31 13:09:11 +0530

    Link Field created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Navbar.js';
 import Banner from './Banner.js';
+import LinkField from './LinkField.js'
 
 function App() {
   return(
     <div>
       <Navbar/>
       <Banner/>
+      <LinkField/>
     </div>
   );
 }
---
+++ src/LinkField.js
@@ -0,0 +1,10 @@
+function LinkField(){
+    return (
+        <div class="link-sec col-lg-8 col-sm-10 mx-auto py-5 px-5 row">
+            <input type="text" placeholder="Enter your link" class="link-field col-lg-10 col-sm-12 py-3 px-3"/>
+            <button type="text" class="btn btn-shorten py-3 px-3 col-lg-2 col-sm-12 mt-sm-4 mt-lg-0 text-white">Shorten It!</button>
+        </div>
+    );
+}
+
+export default LinkField
